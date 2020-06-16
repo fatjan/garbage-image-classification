723cd59ab1ccb85e02a5c3b048f3e81196095852
fatjan <fatma@alterra.com>
2020-06-16 14:32:02 +0000

    [skip ci] Updates

--- store/index.js
+++
@@ -1,8 +0,0 @@
-export const actions = {
-  notifFileTooBig({ dispatch }) {
-    const alertMsg = {
-      msg: 'Ukuran file melebihi batas maksimum.'
-    }
-    dispatch('ui/showAlert', alertMsg, { root: true })
-  }
-}
